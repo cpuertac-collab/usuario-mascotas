f352342d981ac9af341de0815d97ccf598735b2b
Camilo Puerta <cpuertac@unicartagena.edu.co>
2025-11-17 21:11:21 -0500

    Creacion con Spring Boot

---
+++ src/main/java/com/CEA/usuario_mascotas/UsuarioMascotasApplication.java
@@ -0,0 +1,13 @@
+package com.CEA.usuario_mascotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioMascotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioMascotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CEA/usuario_mascotas/UsuarioMascotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CEA.usuario_mascotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioMascotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

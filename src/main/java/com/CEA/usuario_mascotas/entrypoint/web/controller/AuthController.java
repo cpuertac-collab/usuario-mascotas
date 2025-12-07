870a4656859a19c1a95b3ddd05d310980f625d45
Camilo Puerta <cpuertac@unicartagena.edu.co>
2025-12-06 23:45:11 -0500

    Se borraron las vistas Thymeleaf y se agregó el index html + Vue al proyecto Spring

--- src/main/java/com/CEA/usuario_mascotas/entrypoint/web/controller/AuthController.java
+++ src/main/java/com/CEA/usuario_mascotas/entrypoint/web/controller/AuthController.java
@@ -1,13 +1,13 @@
-package com.CEA.usuario_mascotas.entrypoint.web.controller;
+//package com.CEA.usuario_mascotas.entrypoint.web.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class AuthController {
+//@Controller
+//public class AuthController {
 
-    @GetMapping("/login")
-    public String login() {
-        return "usuarios/login";
-    }
-}
+    //@GetMapping("/login")
+    //public String login() {
+    //    return "usuarios/login";
+    //}
+//}
